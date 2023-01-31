bdad82e53704536600b9acdb6a45f1cd61681301
Albino-xx <61127374+Albino-xx@users.noreply.github.com>
2023-01-30 21:02:58 -0600

    Add files via upload

---
+++ ConsoleAppSemana3/Program.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleAppSemana3.clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSemana3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************POO**************");
+
+            Celular miCelular = new Celular();
+            miCelular.Marca = "Apple";
+            miCelular.Modelo = "iPhone 14";
+            miCelular.Color = "Rojo";
+
+            miCelular.llamar("Juan");
+            miCelular.finalizarLlamada();
+
+           
+            Console.WriteLine(miCelular.ToString());
+           
+
+            Celular otroCelular = new Celular("Honor", "Honor Pro", "Negro");
+            Console.WriteLine(otroCelular.ToString());
+
+            Celular Celular3 = new Celular("Cualquier", "Verde");
+            Console.WriteLine(Celular3.ToString());
+
+            Celular Celular4 = new Celular("Iphone", "Iphone 13", "Verde");
+            Console.WriteLine(Celular4.ToString());
+
+            Console.WriteLine("***************************");
+            SmartPhone smartPhone = new SmartPhone("Z flot", "Samsung", "Negro", 180F, 64F);
+            Console.WriteLine(smartPhone.ToString());
+
+            smartPhone.llamar("eso");
+            smartPhone.finalizarLlamada();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppSemana3/clases/Celular.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSemana3.clases
+{
+    public class Celular
+    {
+        private string marca;
+
+        //Atributos
+        public string Marca { get
+            {
+                if (marca == null)
+                {
+                    marca = "Samsung";
+                }
+                return marca;
+            } set => marca = value;  
+        }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+
+        //Constructores
+
+        public Celular() { }
+        public Celular(string marca,string modelo,string color) 
+        { 
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.Color = color;
+        }
+        public Celular(string modelo, string color)
+        {
+            this.Modelo = modelo;
+            this.Color = color;
+        }
+
+        //Metodos
+
+        public void llamar(string nombre)
+        {
+            Console.WriteLine("Lammando a..."+ nombre);
+            Console.WriteLine($"Llamando a {nombre}"); //interpolacion
+            Console.WriteLine("Llamando a {0}", nombre); //Template String
+           
+        }
+
+        public void finalizarLlamada()
+        {
+        Console.WriteLine("Llamada finalizada...");
+        }
+
+        public override string ToString()
+        {
+            string info = $"Marca: {Marca} | Modelo: {Modelo} | Color: {Color}";
+    
+
+            return info;
+        }
+   
+
+    }
+}
